3d039d9b9d240aabee6e5c7e979a772a21d49985
Erika Geraldine <geraldinevaldez120@gmail.com>
2021-11-05 10:17:49 -0300

    arreglo membresia[i], y Hora.cpp

---
+++ Hora_Sistema.cpp
@@ -0,0 +1,13 @@
+#include "Hora_Sistema.h"
+
+void hora_sistema(void){
+	// Inicializacion de hora
+   time_t now = time(0);
+   tm *ltm = localtime(&now);
+   // Impresion y validacion de hora
+   if(ltm->tm_hour>=12){// Si hora es mayor o igual a las 12 pm entonces...
+   	cout<<"\t\t\t                Hora del Sistema: "<<ltm->tm_hour<<":"<<ltm->tm_min<<" pm."<<endl<<endl;
+   }else{// De lo contrario...
+   	cout<<"\t\t\t                Hora del Sistema: "<<ltm->tm_hour<<":"<<ltm->tm_min<<" am."<<endl<<endl;
+   }// Fin if else ltm->tm_hour validacion
+}// Fin funcion mostrar hora
--- Hora_Sistema.h
+++ Hora_Sistema.h
@@ -6,16 +6,6 @@
 #include<ctime>
 using namespace std;
 // Inicio funcion mostrar hora
-void hora_sistema(void){
-	// Inicializacion de hora
-   time_t now = time(0);
-   tm *ltm = localtime(&now);
-   // Impresion y validacion de hora
-   if(ltm->tm_hour>=12){// Si hora es mayor o igual a las 12 pm entonces...
-   	cout<<"\t\t\t                Hora del Sistema: "<<ltm->tm_hour<<":"<<ltm->tm_min<<" pm."<<endl<<endl;
-   }else{// De lo contrario...
-   	cout<<"\t\t\t                Hora del Sistema: "<<ltm->tm_hour<<":"<<ltm->tm_min<<" am."<<endl<<endl;
-   }// Fin if else ltm->tm_hour validacion
-}// Fin funcion mostrar hora
+void hora_sistema(void);
 
 #endif // HORA_SISTEMA_H_INCLUDED
